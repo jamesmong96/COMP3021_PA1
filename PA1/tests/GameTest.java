8e485035f6ce9309216ef3f2c141a3a2fa5e782c
Kin Ip Mong <kimong@connect.ust.hk>
2018-10-22 14:04:40 +0800

    20-10-2018

--- PA1/tests/GameTest.java
+++ PA1/tests/GameTest.java
@@ -1,7 +1,15 @@
 import Exceptions.InvalidMapException;
+import Exceptions.InvalidMatchingOfCrateAndDestTile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.annotation.IncompleteAnnotationException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,7 +46,62 @@ class GameTest {
         assertTrue(g.isWin());
         assertTrue(g.makeMove('r'));
         assertFalse(g.isWin());
+
     }
 
+    @Test
+    @DisplayName("Display - null pointer")
+    void displayNullPointer() {
+
+    }
+
+
+/*    @Test
+    @DisplayName("Reload map with the map disappear")
+    void reloadMapWithTheMapIsGone() throws InvalidMapException, IOException {
+
+        char[][] map = {
+                {'#', '#', '#', '#', '#', '#'},
+                {'#', '.', '.', '.', '.', '#'},
+                {'.', '@', '.', 'a', 'b', '#'},
+                {'#', '.', '.', 'A', 'B', '#'},
+                {'#', '#', '#', '#', '#', '#'},
+        };
+        String filename = "tests/virtualmap.txt";
+        File file;
+        PrintWriter writer = null;
+        int row = map.length;
+        int col = map[0].length;
+
+        if (row < 3)
+            throw new IllegalArgumentException();
+
+        if (col < 3)
+            throw new IllegalArgumentException();
+
+        try {
+            file = new File(filename);
+            writer = new PrintWriter(file);
+
+            writer.println(row);
+            writer.println(col);
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++)
+                    writer.print(map[i][j]);
+                writer.println();
+            }
+
+        } catch (IllegalArgumentException e) {
+            throw e;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            writer.close();
+        }
+
+        g.loadMap("tests/virtualmap.txt");
+        Files.deleteIfExists(Paths.get(new File(".").getAbsolutePath() + File.separator + "tests/virtualmap.txt"));
+        assertThrows(NullPointerException.class, () -> g.makeMove('r'));
+    }*/
 
 }
